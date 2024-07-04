bc5d72bc7abc344dee3204ebe34e624f7d4dd235
anjali <anjaliaher08@gmail.com>
2024-07-04 19:57:33 +0530

    commit

---
+++ day11/program1.java
@@ -0,0 +1,14 @@
+/* To create a array 
+Case1: create array using declare and initialization statement
+Case2: create an array using without initialization
+Case3: create an array using new and initialization */
+
+
+
+
+public class program1 {
+    
+    public static void main(String[] args){
+
+    }
+}

42f5e69341d3ce555d2352ad98c74faa9f4589a3
anjali <anjaliaher08@gmail.com>
2024-07-05 19:30:44 +0530

    commit

---
+++ day6/assignment1/program2.java
@@ -0,0 +1,12 @@
+/* 2.Write a program to find count of prime numbers present  between a given range.
+Constraints : User needs to enter the range (lower bound and upper bound) 
+where lower bound < upper bound and lower bound > 2 . 
+If these conditions are not satisfied then program should print Invalid range.  */
+
+
+
+public class program2 {
+    public static void main(String[] args) {
+        
+    }
+}

b1c1db097234e00241d8b3514c73499f97b6c476
anjali <anjaliaher08@gmail.com>
2024-07-07 13:57:56 +0530

    commit

--- day12/program1.java
+++ day12/program1.java
@@ -1,3 +1,14 @@
 public class program1 {
-    
+    public static int[] getIntArray(int size){
+        //create an array;
+        int[] array = new int[size];
+        //return ref of array object
+        return array;
+    }
+    public static void main(String[] args){
+        int [] v1 = getIntArray(size : 5);
+        System.out.println(v1);
+        System.out.println(v1.length);
+    }
+
 }
